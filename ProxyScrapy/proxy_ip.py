7c6547ca68bac45c310905f6e03328cb821afcb6
walkmanchen <roma1473@163.com>
2018-01-03 15:56:03 +0800

    add code

---
+++ ProxyScrapy/proxy_ip.py
@@ -0,0 +1,116 @@
+# coding=utf8
+"""
+爬取西刺代理网站，但是发现爬取下来的都是垃圾
+想放弃。。。。。
+"""
+import csv
+import random
+import sys
+import telnetlib
+import time
+
+import arrow
+import requests
+from lxml import etree
+
+import setting
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+class Baisc(object):
+    def __init__(self):
+        pass
+
+    def down_load(self, url):
+        headers = {
+                'Connection': 'keep-alive',
+                'Cache-Control': 'max-age=0',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': random.choice(setting.USER_AGENT),
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+        }
+        time.sleep(random.uniform(1, 3))
+        response = requests.get(url, headers=headers)
+        return response
+
+    def save(self, content, save_path):
+        # content_temp = [x.decode('utf-8').encode('GB2312') for x in content]
+        with open(save_path, 'ab+', ) as f:
+            writer = csv.writer(f)
+            writer.writerow(content)
+
+
+class ProxyCrawler(Baisc):
+    def __init__(self):
+        super(Baisc, self).__init__()
+        self.file1 = open('./ip.csv', 'w+')  # 打开文件
+        self.file1.truncate()  # 清空文件内容
+
+    def start(self):
+        for number in range(1, setting.PAGE_NUMBER):
+            url = 'http://www.xicidaili.com/wt/' + str(number)
+            print url
+            print number
+            response = self.down_load(url=url)
+            self.parse(response=response)
+
+
+    def parse(self, response):
+        html = etree.HTML(response.content)
+        # odd标签下
+        ip_list_odd = html.xpath("//tr[@class='odd']/td[2]/text()")
+        port_list_odd  = html.xpath("//tr[@class='odd']/td[3]/text()")
+        speed_list_odd  = html.xpath("//tr[@class='odd']/td[7]/div/@title")
+        live_time_list_odd  = html.xpath("//tr[@class='odd']/td[9]/text()")
+        verify_time = html.xpath("//tr[@class='odd']/td[10]/text()")
+
+        for temp in range(len(ip_list_odd)):
+            current_time = arrow.now()
+            verify_time_each = arrow.get('20' + verify_time[temp] )
+
+            # 转换为秒
+            if '分钟' in live_time_list_odd[temp]:
+                live_time_list_odd[temp] = int(live_time_list_odd[temp][:-2])*60
+            elif '天' in  live_time_list_odd[temp]:
+                live_time_list_odd[temp] = int(live_time_list_odd[temp][:-1])*24*60*60
+            else:
+                live_time_list_odd[temp] = int(live_time_list_odd[temp][:-2])*60*60
+
+
+            if (current_time - verify_time_each).seconds > live_time_list_odd[temp]:
+                print '---2--2-2-2-', (current_time - verify_time_each).seconds
+                print '------', live_time_list_odd[temp]
+                content = [ip_list_odd[temp], port_list_odd[temp], speed_list_odd[temp], live_time_list_odd[temp]]
+                save_path = './ip.csv'
+                self.save(content=content, save_path=save_path)
+
+        # 空值
+        ip_list = html.xpath("//tr[@class='odd']/following-sibling::*[1]/td[2]/text()")
+        port_list = html.xpath("//tr[@class='odd']/following-sibling::*[1]/td[3]/text()")
+        speed_list = html.xpath("//tr[@class='odd']/following-sibling::*[1]/td[7]/div/@title")
+        live_time_list = html.xpath("//tr[@class='odd']/following-sibling::*[1]/td[9]/text()")
+
+        for temp in range(len(ip_list)):
+            content = [ip_list[temp], port_list[temp], speed_list[temp], live_time_list[temp]]
+            save_path = './ip.csv'
+            self.save(content=content, save_path=save_path)
+
+
+    def verify_ip(self):
+        try:
+            telnetlib.Telnet('121.232.145.57', port=9000, timeout=5)
+        except:
+            print '0000'
+        else:
+            print '-----'
+
+
+if __name__ == '__main__':
+
+    proxy_crawler = ProxyCrawler()
+    # proxy_crawler.start()
+    proxy_crawler.verify_ip()
---
+++ ProxyScrapy/setting.py
@@ -0,0 +1,27 @@
+# coding=utf8
+PAGE_NUMBER = 15
+
+USER_AGENT = [
+    'Mozilla/5.0 (iPhone 6s; CPU iPhone OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46 '
+    '(KHTML, like Gecko) Version/9.0 MQQBrowser/7.5.1 Mobile/13G36 Safari/8536.25 MttCusto'
+    'mUA/2 QBWebViewType/1 WKType/1',
+    'Mozilla/5.0 (Linux; U; Android 5.1; zh-cn; m1 metal Build/LMY47I) AppleWebKit/537.36 (KHTML, '
+    'like Gecko)Version/4.0 Chrome/37.0.0.0 MQQBrowser/7.6 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; U; Android 6.0.1; zh-cn; vivo Xplay6 Build/MXB48T) AppleWebKit/537.36 (KHTML,'
+    ' like Gecko)Version/4.0 Chrome/37.0.0.0 MQQBrowser/7.6 Mobile Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML,'
+    ' like Gecko) Mobile/14C92 MicroMessenger/6.5.9 NetType/WIFI Language/zh_CN',
+    'Mozilla/5.0 (Linux; U; Android 4.4.2; zh-CN; HUAWEI MT7-TL00 Build/HuaweiMT7-TL00) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Version/4.0 Chrome/40.0.2214.89 UCBrowser/11.3.8.909 Mobile Safari/537.36',
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, "
+    "like Gecko) Version/5.1 Safari/534.50",
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; '
+    'SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 '
+    '(KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+    'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko)'
+    ' Version/6.0.0.337 Mobile Safari/534.1+',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
+]
